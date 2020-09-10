fc5cf5402e6a09128851611991dea5e74966442b
Kalydy Barnabas <kalydybarnabas99@gmail.com>
2020-09-10 08:12:39 +0200

    tutorial2

---
+++ tutorial2.py
@@ -0,0 +1,10 @@
+#változó típusok: int, float, string
+
+int1 = 5
+float1 = 5.6 #float1 = float(5)
+string1 = "hello world don't worry"
+
+float2 = float(int1) - float1
+
+print(string1)
+print(float2)
