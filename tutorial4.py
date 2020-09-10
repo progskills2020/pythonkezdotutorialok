1c03c9a7c3bd2adede6c7531ff56a6b22d626c68
Kalydy Barnabas <kalydybarnabas99@gmail.com>
2020-09-10 11:44:54 +0200

    tutorial4

---
+++ tutorial4.py
@@ -0,0 +1,49 @@
+"""
+if valami1:
+    ez fut le, ha igaz
+    ez is lefut ha így írjuk
+else:
+    ha nem igaz, akkor ez fut le
+    blabla
+"""
+
+"""
+logikai1 = False
+
+if logikai1:
+    print("az igaz ág futott le")
+else:
+    print("a hamis ág futott le")
+
+if 5>1:
+    print("az igaz ág futott le blabla")
+else:
+    print("a hamis ág futott le blabla")
+
+szam1 = input("elso szam:")
+szam2 = input("második szam:")
+
+if szam1 > szam2:
+    print("az első szám a nagyobb")
+else:
+    print("a második szám a nagyobb vagy egyenlőek")
+"""
+
+#és-and vagy-or
+
+if 5<1 or 14>4:
+    print("legalább az egyik igaz")
+else:
+    print("valamelyik hamis")
+
+#logikai operátorok: == , < , > ,<= , >= , != 
+
+if 2<3 and 3!=3 and 5<6:
+    print("mindhárom igaz")
+else:
+    print("valamelyik nem igaz")
+
+
+
+
+
