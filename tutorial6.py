77889d267e2b5155e6bac646748a9fabef7bfbe9
Kalydy Barnabas <kalydybarnabas99@gmail.com>
2020-09-10 12:20:23 +0200

    tutorial6

---
+++ tutorial6.py
@@ -0,0 +1,17 @@
+
+
+# for x in range(10):
+#     print(x)
+
+"""
+mettől = int(input("mettől írjam ki a számokat?"))
+meddig = int(input("meddig írjam ki a számokat?"))
+
+for x in range(mettől,meddig + 1):
+    print("%d. szám" % x)
+    #itt növeli az x-et 1-gyel
+"""
+
+#itt hármasával számol az 1-től kezdve
+for x in range(1, 10, 3):
+    print(x)
