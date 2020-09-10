20edb2729c23f90eabfe088e14e3cc29bd424e13
Kalydy Barnabas <kalydybarnabas99@gmail.com>
2020-09-10 12:39:42 +0200

    tutorial6

---
+++ tutorial7.py
@@ -0,0 +1,17 @@
+osztalyzatok = [5, 3, 2, 1]
+
+#tomb indexeiből a 0 az első!
+#pl most a 0 indexű elem 5
+#0 indexű elem: osztalyzatok[0]
+
+"""
+for x in range(0, len(osztalyzatok)):
+    print(osztalyzatok[x])
+"""
+
+#visszafelőli kiíratásnál -1-től indul a számolás
+print(osztalyzatok[-2])
+
+osztalyzatok.append(5)
+
+print(osztalyzatok)
