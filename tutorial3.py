e345a00ad5720f25542dc1bb80b93a3826c983bf
Kalydy Barnabas <kalydybarnabas99@gmail.com>
2020-09-10 09:28:37 +0200

    tutorial3

---
+++ tutorial3.py
@@ -0,0 +1,23 @@
+# kommentelés: ctrl+k+c
+# kikommentelés: ctrl+k+u
+
+#boolean változó
+bool1 = True     #igaz
+bool2 = False    #hamis
+
+nev1 = input("Hogy hívnak?")
+
+kor1 = input("Hány éves vagy?")
+
+"""
+%s- string
+%d- int
+%f- float
+"""
+
+print("A te neved %s" % nev1)
+
+print("A te neved %s és %s éves vagy" % (nev1, kor1))
+
+
+
